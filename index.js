3d84b0f03274bbfccd7d41012bc1705f620df490
zablon <9.regular@gmail.com>
2025-12-30 22:10:33 -0800

    added bot

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const HOST = '10.167.131.135'
 const PORT = 25572
 
 const botsConfig = [
-  { host: HOST, port: PORT, name: 'bot_magma_farm', x: 727.7, y: 92, z: 11.5, world: 'world' },
+    { host: HOST, port: PORT, name: 'bot_paper_farm', x: 727.7, y: 92, z: 11.5, world: 'world' },
+    { host: HOST, port: PORT, name: 'bot_magma_farm', x: 289.7, y: 253, z: -220, world: 'nether_world' },
 ]
 
 const bots = botsConfig.map(cfg => new AfkBot(cfg))
