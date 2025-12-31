f7a4c2f1b70adaabbf9fb844e81ec4df552031d0
zablon <9.regular@gmail.com>
2025-12-30 22:11:29 -0800

    fixed name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 25572
 
 const botsConfig = [
     { host: HOST, port: PORT, name: 'bot_paper_farm', x: 727.7, y: 92, z: 11.5, world: 'world' },
-    { host: HOST, port: PORT, name: 'bot_magma_farm', x: 289.7, y: 253, z: -220, world: 'nether_world' },
+    { host: HOST, port: PORT, name: 'bot_magma_farm', x: 289.7, y: 253, z: -220, world: 'world_nether' },
 ]
 
 const bots = botsConfig.map(cfg => new AfkBot(cfg))
