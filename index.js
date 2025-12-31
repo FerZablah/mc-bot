9ff7c6c0e2823cf76dc8d020820a3535566c7b44
zablon <9.regular@gmail.com>
2025-12-30 22:19:43 -0800

    added delay

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const AfkBot = require('./AfkBot') // path to file
 const HOST = '10.167.131.135'
 const PORT = 25572
 
+const SPACING_MS = 15_000
+
 const botsConfig = [
     { host: HOST, port: PORT, name: 'bot_paper_farm', x: 727.7, y: 92, z: 11.5, world: 'world' },
     { host: HOST, port: PORT, name: 'bot_magma_farm', x: 289.7, y: 253, z: -220, world: 'world_nether' },
 ]
 
-const bots = botsConfig.map(cfg => new AfkBot(cfg))
+botsConfig.forEach((cfg, i) => {
+  setTimeout(() => new AfkBot(cfg), i * SPACING_MS)
+})
