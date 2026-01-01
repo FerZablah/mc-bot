01210ee51796e9f82757fe381133464518efa020
zablon <9.regular@gmail.com>
2026-01-01 12:26:20 -0800

    changed names

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = 25572
 const SPACING_MS = 15_000
 
 const botsConfig = [
-    { host: HOST, port: PORT, name: 'bot_paper_farm', x: 727.7, y: 92, z: 11.5, world: 'world' },
-    { host: HOST, port: PORT, name: 'bot_magma_farm', x: 289.7, y: 253, z: -220, world: 'world_nether' },
+    { host: HOST, port: PORT, name: 'Don Juan', x: 727.7, y: 92, z: 11.5, world: 'world' },
+    { host: HOST, port: PORT, name: 'Joaquin', x: 289.7, y: 253, z: -220, world: 'world_nether' },
 ]
 
 botsConfig.forEach((cfg, i) => {
